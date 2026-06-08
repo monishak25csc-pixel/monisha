985553c3ef7c17d3837625ae1289f553e862863a
monisha <monishak25csc@vetias.ac.in>
2026-06-08 14:24:45 +0530

    hi.java

--- src/hi.java
+++ src/hi.java
@@ -1,4 +1,4 @@
-void main(){
+/*void main(){
     int n=5;
     for(int row=1;row<=n;row++){
         for(int column=1;column<=row;column++) {
@@ -6,4 +6,20 @@ void main(){
         }
         System.out.println();
         }
+    }*/
+void main(){
+int[] marks={87,32,44,89,23};
+System.out.println("pass mark");
+for(int mark:marks) {
+    if (mark >= 40) {
+        System.out.println(mark + "");
     }
+}
+    System.out.println("fail marks");
+    for(int mark:marks){
+        if(mark<40){
+            System.out.println(mark+"");
+        }
+    }
+}
+
