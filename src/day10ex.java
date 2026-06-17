173dbaaa6963b2ceee96be92d638f78761b98fe1
monisha <monishak25csc@vetias.ac.in>
2026-06-17 14:19:04 +0530

    java

---
+++ src/day10ex.java
@@ -0,0 +1,21 @@
+public class day10ex{
+    static void main(){
+        String name= "xyz";
+        try {
+            System.out.println(name.charAt(2));
+            System.out.println(20/0);
+        }
+        catch(ArithmeticException e){
+            System.out.println("ArithmeticException");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexoutofbound exception");
+        }
+        catch(Exception e){
+            System.out.println("exception success");
+        }
+        finally {
+            System.out.println("successful excecution");
+        }
+    }
+}
---
+++ src/day11.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+public class day11 {
+    public static void main(String[]args){
+        Set<String> set = new HashSet<>();
+        set.add("moni");
+        set.add("shree");
+        set.add("thanvi");
+        set.add("savi");
+        System.out.println("set");
+        for (String s:set){
+            System.out.println(s);
+
+        }
+
+    }
+
+
+
+
+
+}
